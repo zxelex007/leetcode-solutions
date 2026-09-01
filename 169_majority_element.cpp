94844a1a3404df10a5efbe509e8674c1edff04a4
zxelex007 <107753091+zxelex007@users.noreply.github.com>
2026-09-01 20:47:48 +0530

    Update 169_majority_element.cpp - LeetCode Problem 169

--- 169_majority_element.cpp
+++ 169_majority_element.cpp
@@ -13,5 +13,46 @@ public:
         // Optimised solution -
         std::sort(nums.begin(), nums.end());
         return nums[nums.size() / 2];
+        /* Moore's Voting Algorithm :
+        
+        // Size of the given array
+        int n = nums.size();
+        
+        // Count
+        int cnt = 0;
+        
+        // Element
+        int el; 
+        
+        // Applying the algorithm
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if (el == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        
+        Checking if the stored element
+        is the majority element
+        
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) {
+                cnt1++;
+            }
+        }
+        
+        //return element if it is a majority element
+        if (cnt1 > (n / 2)) {
+            return el;
+        }
+        
+        //return -1 if no such element found
+        return -1;
+        */
     }
 };
